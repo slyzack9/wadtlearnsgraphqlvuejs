9a972c884dc1a6401978c39e141e464e1e99c8b7
Hilmi Sabli <19ftt1154@student.pb.edu.bn>
2021-05-20 10:20:26 +0800

    changes vue

--- my-app/src/components/App.vue
+++ my-app/src/components/App.vue
@@ -52,6 +52,7 @@
                 </td>
                 <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                   {{ person.role }}
+                  
                 </td>
                 <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                   <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
