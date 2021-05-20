b80b0e2e2218d1c8707b951a5b44229e8de0c1cd
Hilmi Sabli <19ftt1154@student.pb.edu.bn>
2021-05-20 09:55:30 +0800

    app vue changes

--- my-app/src/components/App.vue
+++ my-app/src/components/App.vue
@@ -1,109 +1,89 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-  <div id="app">
-    <p class="username">{{ currentUser.username }}'s posts:</p>
-    <ul>
-      <li v-for="post in posts" :key="post.id">{{ post.content }}</li>
-    </ul>
-    <div>
-      <input v-model="newPostContent" />
-      <button @click="addPost()">Add Post</button>
+  <div class="flex flex-col">
+    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+      <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+          <table class="min-w-full divide-y divide-gray-200">
+            <thead class="bg-gray-50">
+              <tr>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Name
+                </th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Title
+                </th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Status
+                </th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Role
+                </th>
+                <th scope="col" class="relative px-6 py-3">
+                  <span class="sr-only">Edit</span>
+                </th>
+              </tr>
+            </thead>
+            <tbody class="bg-white divide-y divide-gray-200">
+              <tr v-for="person in people" :key="person.email">
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="flex items-center">
+                    <div class="flex-shrink-0 h-10 w-10">
+                      <img class="h-10 w-10 rounded-full" :src="person.image" alt="" />
+                    </div>
+                    <div class="ml-4">
+                      <div class="text-sm font-medium text-gray-900">
+                        {{ person.name }}
+                      </div>
+                      <div class="text-sm text-gray-500">
+                        {{ person.email }}
+                      </div>
+                    </div>
+                  </div>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="text-sm text-gray-900">{{ person.title }}</div>
+                  <div class="text-sm text-gray-500">{{ person.department }}</div>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                    Active
+                  </span>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                  {{ person.role }}
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                  <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import gql from "graphql-tag";
-
-const CURRENT_USER = gql`
-  query {
-    currentUser {
-      id
-      username
-    }
-  }
-`;
-
-const POSTS_BY_USER = gql`
-  query($userId: String!) {
-    postsByUser(userId: $userId) {
-      id
-      content
-    }
-  }
-`;
-
-const ADD_POST = gql`
-  mutation($content: String!) {
-    addPost(content: $content) {
-      id
-      content
-    }
-  }
-`;
-
-function updateAddPost(cache, result) {
-  let newPost = result.data.addPost;
-
-  console.log(newPost.id);
-
-  let cacheId = {
-    query: POSTS_BY_USER,
-    variables: { userId: this.currentUser.id }
-  };
-
-  const data = cache.readQuery(cacheId);
-  const newData = [...data.postsByUser, newPost];
-
-  cache.writeQuery({
-    ...cacheId,
-    data: { postsByUser: newData }
-  });
-}
+const people = [
+  {
+    name: 'Jane Cooper',
+    title: 'Regional Paradigm Technician',
+    department: 'Optimization',
+    role: 'Admin',
+    email: 'jane.cooper@example.com',
+    image:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+  },
+  // More people...
+]
 
 export default {
-  name: "app",
-  data: function() {
+  setup() {
     return {
-      currentUser: { username: "user" },
-      posts: [],
-      newPostContent: ""
-    };
-  },
-
-  methods: {
-    addPost() {
-      this.$apollo.mutate({
-        mutation: ADD_POST,
-        variables: { content: this.newPostContent },
-        update: updateAddPost.bind(this),
-
-        // NEW
-        optimisticResponse: {
-          __typename: "Mutation",
-          addPost: {
-            __typename: "Post",
-            id: "xyz-?",
-            content: this.newPostContent,
-            userId: this.currentUser.id
-          }
-        }
-      });
-
-      this.newPostContent = "";
+      people,
     }
   },
-
-  apollo: {
-    currentUser: CURRENT_USER,
-    posts: {
-      query: POSTS_BY_USER,
-      variables() {
-        return { userId: String(this.currentUser.id) };
-      },
-      update(data) {
-        return data.postsByUser;
-      }
-    }
-  }
-};
+}
 </script>
